747c4e03e13fc24971e1ef3c55f89959397fb081
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2021-11-06 20:03:56 +0300

    init ts

---
+++ main.ts
@@ -0,0 +1,18 @@
+require('dotenv').config();
+import { Telegraf } from "telegraf";
+
+if (process.env.BOT_TOKEN === undefined) {
+    throw new Error('BOT_TOKEN must be provided!');
+}
+
+const bot = new Telegraf(process.env.BOT_TOKEN)
+bot.start(ctx => {
+    ctx.reply('Welcome');
+    ctx.reply(String(ctx.botInfo.id));
+})
+
+bot.launch()
+
+// Enable graceful stop
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
