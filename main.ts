2b87ec65a10582313ddb64b445eb0d669e932363
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2021-11-07 13:03:40 +0300

    init eslint & prettier

--- main.ts
+++ main.ts
@@ -1,18 +1,18 @@
-require('dotenv').config();
-import { Telegraf } from "telegraf";
+import 'dotenv/config';
+import { Telegraf } from 'telegraf';
 
-if (process.env.BOT_TOKEN === undefined) {
-    throw new Error('BOT_TOKEN must be provided!');
+if (!process.env.BOT_TOKEN) {
+  throw new Error('BOT_TOKEN must be provided!');
 }
 
-const bot = new Telegraf(process.env.BOT_TOKEN)
-bot.start(ctx => {
-    ctx.reply('Welcome');
-    ctx.reply(String(ctx.botInfo.id));
-})
+const bot = new Telegraf(process.env.BOT_TOKEN);
+bot.start((ctx) => {
+  ctx.reply('Welcome');
+  ctx.reply(String(ctx.botInfo.id));
+});
 
-bot.launch()
+bot.launch();
 
 // Enable graceful stop
-process.once('SIGINT', () => bot.stop('SIGINT'))
-process.once('SIGTERM', () => bot.stop('SIGTERM'))
+process.once('SIGINT', () => bot.stop('SIGINT'));
+process.once('SIGTERM', () => bot.stop('SIGTERM'));
