ea302f20e9e90972d83abff7ab8c5f10223fe87e
Ivan Tyulkin <phantomlancerinyourmind@gmail.com>
2021-12-02 19:09:19 +0300

    [swap] api url

--- src/const.ts
+++ src/const.ts
@@ -1,2 +1,1 @@
-// export const API_URL = 'https://fierce-ridge-30370.herokuapp.com/';
-export const API_URL = 'http://localhost:5000/';
+export const API_URL = 'https://fierce-ridge-30370.herokuapp.com/';
